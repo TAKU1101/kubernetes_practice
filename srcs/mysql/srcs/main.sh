6f468f1be2ed33beec984fb59ff8c170e2ba7fe0
TAKU1101 <taku.kuma1101@icloud.com>
2021-03-04 10:23:38 +0100

    Add: mysql

---
+++ srcs/mysql/srcs/main.sh
@@ -0,0 +1,14 @@
+mysql_install_db
+
+mkdir -p run/mysqld/
+touch run/mysqld/mysqld.sock
+mysqld -u root & sleep 3
+
+mysql -e "CREATE DATABASE wpdb;"
+mysql -e "CREATE USER 'wpuser'@'localhost' identified by 'dbpassword';"
+mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# mysql -u root
+
+tail -f /dev/null
