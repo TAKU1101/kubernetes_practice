41e5c97120bab9a12e7544942aab73cb8706aab8
TAKU1101 <taku.kuma1101@icloud.com>
2021-03-16 11:49:11 +0100

    Add: grafana datafile

--- srcs/grafana/srcs/main.sh
+++ srcs/grafana/srcs/main.sh
@@ -1,3 +1,4 @@
 telegraf &
 
-grafana-server --config=/etc/grafana.ini --homepath=/usr/share/grafana
+# grafana-server --config=/etc/grafana.ini --homepath=/usr/share/grafana
+grafana-server --homepath=/usr/share/grafana
