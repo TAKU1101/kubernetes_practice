4f65400419264f564b9edbd99481b674c0daf693
TAKU1101 <taku.kuma1101@icloud.com>
2021-03-19 09:03:25 +0100

    Add: nginx

--- srcs/phpmyadmin/srcs/main.sh
+++ srcs/phpmyadmin/srcs/main.sh
@@ -1,5 +1,8 @@
 # telegraf start (deamon)
 telegraf &
 
+# nginx start
+nginx
+
 # php start (-S: start built-in web server in port, -t: Built-in web server directory) 
 /usr/bin/php -S 0.0.0.0:5000 -t /var/www/phpmyadmin
--- srcs/wordpress/srcs/main.sh
+++ srcs/wordpress/srcs/main.sh
@@ -1,5 +1,8 @@
 # telegraf start (deamon)
 telegraf &
 
+# nginx start
+nginx
+
 # php start (-S: start built-in web server in port, -t: Built-in web server directory) 
 php -S 0.0.0.0:5050 -t /var/www/wordpress/
